4fa99e78bf3c70551ec6e7b9f016fb3d2ffd01aa
Steelcow85 <m.w.goodwin@gmail.com>
2016-09-07 12:28:36 -0400

    new video mp4

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var vid;
 var ctrl=false;
 function setup() {
   createCanvas(0,0);
-  vid=createVideo("countdown_android_3gpp_lrg.3gp");
+  vid=createVideo("countdown_android_mp4_480.mp4");
   vid.loop();
 
 }
