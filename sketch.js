b29c381f1163de137612ae8b143e6f6b43e8defe
Steelcow85 <m.w.goodwin@gmail.com>
2016-09-07 12:50:51 -0400

    new vid

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var vid;
 var ctrl=false;
 function setup() {
   createCanvas(480,360);
-  vid=createVideo("countdown_mpeg4.mp4");
+  vid = createVideo("countdown_mp4_480.mp4");
   vid.loop();
   vid.hide();
 
